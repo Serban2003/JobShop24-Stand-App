1d8ede14317310c8a6e8d69dbc0f88d082563891
Serban <iustinianserban@gmail.com>
2024-02-26 17:56:46 +0200

    Implement unselection of stand Rewrite code because it sucked

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBluoOT6jdcY3TiIioEPCj67Ot3Bj4YPCo",
-    authDomain: "jobshop-stand-app.firebaseapp.com",
-    projectId: "jobshop-stand-app",
-    storageBucket: "jobshop-stand-app.appspot.com",
-    messagingSenderId: "125142518927",
-    appId: "1:125142518927:web:7dac2c7cb74a6d3301b4fa",
-    measurementId: "G-48VB3QDNKW",
-    databaseURL: "https://jobshop-stand-app-default-rtdb.europe-west1.firebasedatabase.app/",
+  apiKey: "AIzaSyBluoOT6jdcY3TiIioEPCj67Ot3Bj4YPCo",
+  authDomain: "jobshop-stand-app.firebaseapp.com",
+  projectId: "jobshop-stand-app",
+  storageBucket: "jobshop-stand-app.appspot.com",
+  messagingSenderId: "125142518927",
+  appId: "1:125142518927:web:7dac2c7cb74a6d3301b4fa",
+  measurementId: "G-48VB3QDNKW",
+  databaseURL: "https://jobshop-stand-app-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
@@ -19,8 +19,11 @@ const database = firebase.database();
 var companyID = '';
 var companyName = '';
 var companyStand = 0;
+var nrStands = 4;
+var standKeys = '';
+var standInfo = '';
 
-firebase.auth().onAuthStateChanged(function(company) {
+firebase.auth().onAuthStateChanged(function (company) {
 
   if (company) {
     console.log(company.email);
@@ -29,30 +32,51 @@ firebase.auth().onAuthStateChanged(function(company) {
     document.getElementById("log-in-message").style.display = "block";
 
     companyID = company.email.substring(0, company.email.indexOf('@'));
-    retrieveCompanyData(companyID);
+
+    /* Retrieve company details */
+    var companyDetails = database.ref('company/' + companyID);
+
+    companyDetails.once('value', (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        companyName = data.name;
+        companyStand = data.stand;
+      }
+    }).then(function () {
+      document.getElementById("companyName").innerHTML = companyName;
+
+      /* Retrieve stand info */
+      var stands = database.ref('stands/');
+
+      stands.once('value', (snapshot) => {
+        const data = snapshot.val();
+
+        standInfo = Object.values(data);
+        standKeys = Object.keys(data);
+      }).then(
+        function () {
+          console.log(standInfo);
+          console.log(standKeys);
+          updateStands();
+        }
+      );
+    });
+
+
   } else {
     console.log("No company connected");
     document.getElementById("log-out-button").style.display = "none";
     document.getElementById("log-in-button").style.display = "block";
     document.getElementById("log-in-message").style.display = "none";
-  }
-  
-  retrieveStandInfo();
-});
 
-function retrieveCompanyData(ID){
-  var companyDetails = database.ref('company/' + ID);
+    /* Retrieve stand info */
+    var stands = database.ref('stands/');
 
-  companyDetails.once('value', (snapshot) => {
+    stands.once('value', (snapshot) => {
       const data = snapshot.val();
-      if(data){
-        companyName = data.name;
-        companyStand = data.stand;
-      }
-  }).then(updateData);
-}
 
-function updateData(){
-  document.getElementById("companyName").innerHTML = companyName;
-}
-
+      standInfo = Object.values(data);
+      standKeys = Object.keys(data);
+    });
+  }
+});
--- scripts/modify_stand.js
+++ scripts/modify_stand.js
@@ -1,36 +1,120 @@
 function selectStand(number) {
-    if (companyID == ''){
+    if (companyID == '') {
         appendAlert("You are not connected!", "You must be connected in order to select a stand!", "danger");
         return;
     }
-    if (companyStand != 0){
-        appendAlert("You have already chosen a stand!", "If you want to choose another one, delete your curent stand and then try again.", "danger");
-        return;
+    if (companyStand != 0) {
+        if (companyStand == number) {
+            $('#deleteModal').modal('show');
+            return;
+        } else {
+            appendAlert("You have already chosen a stand!", "If you want to choose another one, delete your curent stand and then try again.", "danger");
+            return;
+        }
     }
 
-    firebase.database().ref('stands/' + number).set({
-        company: companyName,
-    }).then(retrieveStandInfo);
-
+    //Update databse company data
     firebase.database().ref('company/' + companyID).set({
         name: companyName,
-        stand: number 
-    }).then(retrieveStandInfo);
+        stand: number
+    }).then(function () {
+        //Update database stand info
+        firebase.database().ref('stands/' + number).set({
+            company: companyName,
+        }).then(function () {
+            var companyDetails = database.ref('company/' + companyID);
+            companyDetails.once('value', (snapshot) => {
+                const data = snapshot.val();
+                if (data) {
+                    companyName = data.name;
+                    companyStand = data.stand;
+                }
+            }).then(function () {
+                /* Retrieve stand info */
+                var stands = database.ref('stands/');
+
+                stands.once('value', (snapshot) => {
+                    const data = snapshot.val();
+
+                    standInfo = Object.values(data);
+                    standKeys = Object.keys(data);
+                }).then(
+                    function () {
+                        console.log(standInfo);
+                        console.log(standKeys);
+                        updateStands();
+                    }
+                );
+            });
+        });
+    });
 }
+
+function updateStands() {
+    console.log(standInfo);
+    for (var i = 1; i <= nrStands; ++i) {
+        var stand = document.getElementById("stand-" + i);
+
+        if (standKeys.includes(String(i))) {
+            if (i == companyStand) {
+                stand.classList.remove("occupied-stand");
+                stand.classList.add("selected-stand");
+                console.log(i);
+            } else {
+                stand.classList.add("ocuppied-stand");
+                stand.classList.remove("selected-stand");
+            }
+            stand.innerHTML = standInfo[standKeys.indexOf(String(i))].company;
+        } else {
+            stand.classList.remove("occupied-stand");
+            stand.classList.remove("selected-stand");
+            stand.innerHTML = "Stand " + i;
+        }
+    }
+}
+
 
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, description, type) => {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = [
         `<div class="alert alert-${type} alert-dismissible m-3" role="alert">`,
-        
+
         `<h4 class="alert-heading">Warning!</h4>`,
         `   <p>${message}</p>`,
         `<hr>`,
         `<p class="mb-0">${description}</p>`,
         `<button type="button" class="btn btn-close" data-bs-dismiss="alert"></button>`,
         `</div>`
-    ].join('')
+    ].join('');
 
     alertPlaceholder.append(wrapper)
+}
+
+function unselectStand() {
+    firebase.database().ref('stands/').child(companyStand).remove().then(function () {
+        //Update database company data
+        firebase.database().ref('company/' + companyID).set({
+            name: companyName,
+            stand: 0
+        }).then(function () {
+            /* Retrieve stand info */
+            var stands = database.ref('stands/');
+
+            stands.once('value', (snapshot) => {
+                const data = snapshot.val();
+
+                standInfo = Object.values(data);
+                standKeys = Object.keys(data);
+            }).then(
+                function () {
+                    console.log(standInfo);
+                    console.log(standKeys);
+                    updateStands();
+                    $('#deleteModal').modal('hide');
+                }
+            );
+
+        });
+    });
 }
